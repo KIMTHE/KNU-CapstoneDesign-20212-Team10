a3336af0241bcba5f29d86fff5a3b31df09ea685
wlals761 <wlals761@naver.com>
2021-11-25 23:39:14 +0900

    [FEAT] : Add a cafe search api
    현재 위치를 기반으로 주위 카페 검색 후 마커를 찍음
    주위 카페 범위 설정 가능(m단위)

--- android/app/src/main/java/com/jongsip/cafe/fragment/MapFragment.kt
+++ android/app/src/main/java/com/jongsip/cafe/fragment/MapFragment.kt
@@ -1,9 +1,11 @@
 package com.jongsip.cafe.fragment
+import KakaoAPI
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.*
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,20 +14,38 @@ import android.widget.*
 import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jongsip.cafe.R
+import com.jongsip.cafe.model.ResultSearchKeyword
 import com.jongsip.cafe.util.PermissionUtils
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import java.util.*
 
 
 class MapFragment : Fragment() {
     lateinit var btnMoveHere: FloatingActionButton
     private lateinit var mapView : MapView
     lateinit var locatioNManager : LocationManager
+    lateinit var mapViewContainer: ViewGroup
+    var  urlList = arrayOfNulls<String>(15)//카페 url 담을 배열
 
     var longitude : Double = 0.0
     var latitude : Double = 0.0
 
+    companion object {
+        const val BASE_URL = "https://dapi.kakao.com/"
+        const val API_KEY = "KakaoAK 84a60e48f5913e29b5d156b3a15da41f"  // REST API 키
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -34,7 +54,7 @@ class MapFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_map, container, false)
         mapView = MapView(activity)
-        val mapViewContainer: ViewGroup = rootView.findViewById(R.id.map_view)
+        mapViewContainer = rootView.findViewById(R.id.map_view)
         mapViewContainer.addView(mapView)
 
         btnMoveHere = rootView.findViewById(R.id.btn_move_here)
@@ -49,6 +69,8 @@ class MapFragment : Fragment() {
                 Toast.makeText(activity, "GPS를 켜주세요", Toast.LENGTH_SHORT).show()
             }
         }
+
+        btnMoveHere.performClick()
 
         return rootView
     }
@@ -65,7 +87,29 @@ class MapFragment : Fragment() {
         if(userLocation != null){
             latitude = userLocation.latitude
             longitude = userLocation.longitude
-            Toast.makeText(activity, "현재 내 위치 값: ${latitude}, ${longitude}", Toast.LENGTH_SHORT).show()
+
+            var mapPoint : MapPoint = MapPoint.mapPointWithGeoCoord(latitude, longitude)
+            mapView.setMapCenterPoint(mapPoint, true)//맵 이동
+            mapView.setZoomLevel(1, true)
+            searchKeyword("카페")
+        }
+    }
+
+    //지도에 마커 추가
+    private fun addMarker(response: Response<ResultSearchKeyword>) {
+        val marker = MapPOIItem()
+        var mapPoint : MapPoint
+        var i : Int = 0
+
+        for (item in response.body()?.documents!!) {
+            urlList[i] = item.place_url
+            i += 1
+            marker.itemName = item.place_name
+            mapPoint = MapPoint.mapPointWithGeoCoord(item.y.toDouble(), item.x.toDouble())
+            marker.mapPoint = mapPoint
+            marker.markerType = MapPOIItem.MarkerType.BluePin
+            marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
+            mapView.addPOIItem(marker)
         }
     }
 
@@ -83,11 +127,36 @@ class MapFragment : Fragment() {
         }else{
             currentLatLng = getLatLng()
         }
-
         return currentLatLng
     }
 
-    companion object {
+    private fun searchKeyword(keyword: String) {
+        val retrofit = Retrofit.Builder()   // Retrofit 구성
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(KakaoAPI::class.java)   // 통신 인터페이스를 객체로 생성
+        val call = api.getSearchKeyword(API_KEY, keyword,longitude.toString(), latitude.toString(), 500)   // 검색 조건 입력
 
+        // API 서버에 요청
+        call.enqueue(object: Callback<ResultSearchKeyword> {
+            override fun onResponse(
+                call: Call<ResultSearchKeyword>,
+                response: Response<ResultSearchKeyword>
+            ) {
+                // 통신 성공 (검색 결과는 response.body()에 담겨있음)
+                Log.d("Test", "Raw: ${response.raw()}")
+                Log.d("Test", "Body: ${response.body()?.documents}")
+                addMarker(response)
+            }
+            override fun onFailure(call: Call<ResultSearchKeyword>, t: Throwable) {
+                // 통신 실패
+                Log.w("MainActivity", "통신 실패: ${t.message}")
+            }
+        })
+    }
+    override fun onStop() {
+        super.onStop()
+        mapViewContainer.removeView(mapView)
     }
 }
